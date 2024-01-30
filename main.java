219c0b30fe6bd653318ac651f61ce98a55f7a1d7
stapanuu <118304510+stapanuu@users.noreply.github.com>
2024-01-30 11:00:30 +0200

    Update main.java

--- main.java
+++ main.java
@@ -1,1 +1,89 @@
-sergsergsegregsgfdsgfsfg
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+class Book {
+    private String title;
+    private String author;
+    private String ISBN;
+
+    public Book(String title, String author, String ISBN) {
+        this.title = title;
+        this.author = author;
+        this.ISBN = ISBN;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    @Override
+    public String toString() {
+        return "Title: " + title + ", Author: " + author + ", ISBN: " + ISBN;
+    }
+}
+
+class Library {
+    private List<Book> books;
+
+    public Library() {
+        this.books = new ArrayList<>();
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+        System.out.println("Book added to the library: " + book.getTitle());
+    }
+
+    public void removeBook(String ISBN) {
+        Iterator<Book> iterator = books.iterator();
+        while (iterator.hasNext()) {
+            Book book = iterator.next();
+            if (book.getISBN().equals(ISBN)) {
+                iterator.remove();
+                System.out.println("Book removed from the library: " + book.getTitle());
+                return;
+            }
+        }
+        System.out.println("Book with ISBN " + ISBN + " not found in the library.");
+    }
+
+    public void displayBooks() {
+        if (books.isEmpty()) {
+            System.out.println("No books in the library.");
+        } else {
+            System.out.println("Books in the library:");
+            for (Book book : books) {
+                System.out.println(book);
+            }
+        }
+    }
+}
+
+public class main {
+    public static void main(String[] args) {
+        Library library = new Library();
+
+        Book book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", "978-3-16-148410-0");
+        Book book2 = new Book("To Kill a Mockingbird", "Harper Lee", "978-0-06-112008-4");
+        Book book3 = new Book("1984", "George Orwell", "978-0-452-28423-4");
+
+        library.addBook(book1);
+        library.addBook(book2);
+        library.addBook(book3);
+
+        library.displayBooks();
+
+        library.removeBook("978-0-06-112008-4");
+
+        library.displayBooks();
+    }
+}
